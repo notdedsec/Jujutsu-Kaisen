f6a0fb673e8780f08b98b3adbe71c3b937408bf0
notdedsec <notdedsec@gmail.com>
2021-02-04 16:36:37 +0530

    update encode scripts

--- 04/jujutsu_04.py
+++ 04/jujutsu_04.py
@@ -1,3 +1,4 @@
+#!C:/KaizokuEncoder/python
 import os
 import acsuite
 import subprocess
@@ -6,7 +7,7 @@ import vapoursynth as vs
 ed = 30570
 source =  __file__[:-3]+'.mkv'
 premux =  __file__[:-3]+'_premux.mkv'
-encode_args = '--crf 15 --preset veryslow --qcomp 0.8 --aq-mode 3 --aq-strength 0.9 --psy-rd 0.85:0.0 --keyint 280 \
+encode_args = '--crf 16 --preset veryslow --qcomp 0.8 --aq-mode 3 --aq-strength 0.9 --psy-rd 0.85:0.0 --keyint 280 \
     --ref 16 --bframes 16 --rc-lookahead 72 --me tesa --merange 32 --subme 10 --output-depth 10 --deblock -2:-2 \
     --fps 24000/1001 --colormatrix bt709 --colorprim bt709 --transfer bt709 --no-dct-decimate --no-fast-pskip'
 
@@ -34,4 +35,5 @@ encode_process.communicate()
 
 premux_cmd = ['mkvmerge', '-o', premux, video_out, audio_out]
 subprocess.run(premux_cmd)
-
+os.remove(video_out)
+os.remove(audio_out)
