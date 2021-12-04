e7dfd43de646ed8067c6cc12e91fd147cee53beb
notdedsec <notdedsec@gmail.com>
2021-12-05 02:21:35 +0530

    recreate OP1E

--- BDMV/NC.py
+++ BDMV/NC.py
@@ -1,15 +1,17 @@
 import os
-from vardautomation import FileInfo, PresetFLAC, PresetBD
+from . import custom
+from vardautomation import FileInfo, PresetAAC, PresetBD
 
 BDMV = os.path.dirname(__file__)
 
-OP1  = FileInfo(fr'{BDMV}/JUJUTSUKAISEN_1/BDMV/STREAM/00005.m2ts', (0, -24), preset=[PresetBD, PresetFLAC])
-ED1  = FileInfo(fr'{BDMV}/JUJUTSUKAISEN_1/BDMV/STREAM/00006.m2ts', (0, -24), preset=[PresetBD, PresetFLAC])
-OP2  = FileInfo(fr'{BDMV}/JUJUTSUKAISEN_5/BDMV/STREAM/00005.m2ts', (0, -24), preset=[PresetBD, PresetFLAC])
-ED2  = FileInfo(fr'{BDMV}/JUJUTSUKAISEN_5/BDMV/STREAM/00018.m2ts', (0, -24), preset=[PresetBD, PresetFLAC])
+OP1  = FileInfo(fr'{BDMV}/JUJUTSUKAISEN_1/BDMV/STREAM/00005.m2ts', (0, -24), preset=[PresetBD, PresetAAC])
+ED1  = FileInfo(fr'{BDMV}/JUJUTSUKAISEN_1/BDMV/STREAM/00006.m2ts', (0, -24), preset=[PresetBD, PresetAAC])
+OP2  = FileInfo(fr'{BDMV}/JUJUTSUKAISEN_5/BDMV/STREAM/00005.m2ts', (0, -24), preset=[PresetBD, PresetAAC])
+ED2  = FileInfo(fr'{BDMV}/JUJUTSUKAISEN_5/BDMV/STREAM/00018.m2ts', (0, -24), preset=[PresetBD, PresetAAC])
 
-OP1B = FileInfo(fr'{BDMV}/JUJUTSUKAISEN_1/BDMV/STREAM/00010.m2ts', (0, -24), preset=[PresetBD, PresetFLAC])
-OP1C = FileInfo(fr'{BDMV}/JUJUTSUKAISEN_2/BDMV/STREAM/00005.m2ts', (0, -24), preset=[PresetBD, PresetFLAC])
-OP1D = FileInfo(fr'{BDMV}/JUJUTSUKAISEN_3/BDMV/STREAM/00005.m2ts', (0, -24), preset=[PresetBD, PresetFLAC])
+OP1B = FileInfo(fr'{BDMV}/JUJUTSUKAISEN_1/BDMV/STREAM/00010.m2ts', (0, -24), preset=[PresetBD, PresetAAC])
+OP1C = FileInfo(fr'{BDMV}/JUJUTSUKAISEN_2/BDMV/STREAM/00005.m2ts', (0, -24), preset=[PresetBD, PresetAAC])
+OP1D = FileInfo(fr'{BDMV}/JUJUTSUKAISEN_3/BDMV/STREAM/00005.m2ts', (0, -24), preset=[PresetBD, PresetAAC])
 
-OP1E = FileInfo(fr'{BDMV}/JUJUTSUKAISEN_3/BDMV/STREAM/00005.m2ts', (0, -24), preset=[PresetBD, PresetFLAC]) # TODO create this
+OP1E = FileInfo(fr'{BDMV}/JUJUTSUKAISEN_3/BDMV/STREAM/00005.m2ts', (0, -24), preset=[PresetBD, PresetAAC])
+OP1E.clip_cut = custom.recreate_OP1E()
