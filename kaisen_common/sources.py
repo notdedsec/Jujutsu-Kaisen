7ef13261f4e2b5d0a7cf4514cd45dc35310ca8e5
dedsec <notdedsec@gmail.com>
2023-07-01 01:50:02 +0530

    update sources

--- kaisen_common/sources.py
+++ kaisen_common/sources.py
@@ -1,61 +1,58 @@
-from vardautomation import FileInfo, PresetAAC, PresetBD
-from .custom import recreate_OP1E
+import os
 
-BDMV = '../BDMV'
+from vardautomation import FileInfo, VPath, PresetAAC, PresetBD
+from .utils import recreate_OP1E
+
+BDMV = VPath(os.path.dirname(__file__)).parent / 'BDMV'
+
 
 # Episodes
 
-E01 = FileInfo(fr'{BDMV}/JUJUTSUKAISEN_1/BDMV/STREAM/00002.m2ts', (24, None), preset=[PresetBD, PresetAAC])
-E02 = FileInfo(fr'{BDMV}/JUJUTSUKAISEN_1/BDMV/STREAM/00003.m2ts', (26, None), preset=[PresetBD, PresetAAC])
-E03 = FileInfo(fr'{BDMV}/JUJUTSUKAISEN_1/BDMV/STREAM/00004.m2ts', (26, -27),  preset=[PresetBD, PresetAAC])
+E01 = FileInfo(BDMV / 'JUJUTSUKAISEN_1/BDMV/STREAM/00002.m2ts', (24, None), preset=[PresetBD, PresetAAC])
+E02 = FileInfo(BDMV / 'JUJUTSUKAISEN_1/BDMV/STREAM/00003.m2ts', (26, None), preset=[PresetBD, PresetAAC])
+E03 = FileInfo(BDMV / 'JUJUTSUKAISEN_1/BDMV/STREAM/00004.m2ts', (26, -27),  preset=[PresetBD, PresetAAC])
 
-E04 = FileInfo(fr'{BDMV}/JUJUTSUKAISEN_2/BDMV/STREAM/00002.m2ts', (24, None), preset=[PresetBD, PresetAAC])
-E05 = FileInfo(fr'{BDMV}/JUJUTSUKAISEN_2/BDMV/STREAM/00003.m2ts', (26, None), preset=[PresetBD, PresetAAC])
-E06 = FileInfo(fr'{BDMV}/JUJUTSUKAISEN_2/BDMV/STREAM/00004.m2ts', (27, -24),  preset=[PresetBD, PresetAAC])
+E04 = FileInfo(BDMV / 'JUJUTSUKAISEN_2/BDMV/STREAM/00002.m2ts', (24, None), preset=[PresetBD, PresetAAC])
+E05 = FileInfo(BDMV / 'JUJUTSUKAISEN_2/BDMV/STREAM/00003.m2ts', (26, None), preset=[PresetBD, PresetAAC])
+E06 = FileInfo(BDMV / 'JUJUTSUKAISEN_2/BDMV/STREAM/00004.m2ts', (27, -24),  preset=[PresetBD, PresetAAC])
 
-E07 = FileInfo(fr'{BDMV}/JUJUTSUKAISEN_3/BDMV/STREAM/00002.m2ts', (24, None), preset=[PresetBD, PresetAAC])
-E08 = FileInfo(fr'{BDMV}/JUJUTSUKAISEN_3/BDMV/STREAM/00003.m2ts', (27, None), preset=[PresetBD, PresetAAC])
-E09 = FileInfo(fr'{BDMV}/JUJUTSUKAISEN_3/BDMV/STREAM/00004.m2ts', (26, -25),  preset=[PresetBD, PresetAAC])
+E07 = FileInfo(BDMV / 'JUJUTSUKAISEN_3/BDMV/STREAM/00002.m2ts', (24, None), preset=[PresetBD, PresetAAC])
+E08 = FileInfo(BDMV / 'JUJUTSUKAISEN_3/BDMV/STREAM/00003.m2ts', (27, None), preset=[PresetBD, PresetAAC])
+E09 = FileInfo(BDMV / 'JUJUTSUKAISEN_3/BDMV/STREAM/00004.m2ts', (26, -25),  preset=[PresetBD, PresetAAC])
 
-E10 = FileInfo(fr'{BDMV}/JUJUTSUKAISEN_4/BDMV/STREAM/00002.m2ts', (24, None), preset=[PresetBD, PresetAAC])
-E11 = FileInfo(fr'{BDMV}/JUJUTSUKAISEN_4/BDMV/STREAM/00003.m2ts', (25, None), preset=[PresetBD, PresetAAC])
-E12 = FileInfo(fr'{BDMV}/JUJUTSUKAISEN_4/BDMV/STREAM/00004.m2ts', (27, -25),  preset=[PresetBD, PresetAAC])
+E10 = FileInfo(BDMV / 'JUJUTSUKAISEN_4/BDMV/STREAM/00002.m2ts', (24, None), preset=[PresetBD, PresetAAC])
+E11 = FileInfo(BDMV / 'JUJUTSUKAISEN_4/BDMV/STREAM/00003.m2ts', (25, None), preset=[PresetBD, PresetAAC])
+E12 = FileInfo(BDMV / 'JUJUTSUKAISEN_4/BDMV/STREAM/00004.m2ts', (27, -25),  preset=[PresetBD, PresetAAC])
 
-E13 = FileInfo(fr'{BDMV}/JUJUTSUKAISEN_5/BDMV/STREAM/00002.m2ts', (24, None), preset=[PresetBD, PresetAAC])
-E14 = FileInfo(fr'{BDMV}/JUJUTSUKAISEN_5/BDMV/STREAM/00003.m2ts', (26, None), preset=[PresetBD, PresetAAC])
-E15 = FileInfo(fr'{BDMV}/JUJUTSUKAISEN_5/BDMV/STREAM/00004.m2ts', (26, -27),  preset=[PresetBD, PresetAAC])
+E13 = FileInfo(BDMV / 'JUJUTSUKAISEN_5/BDMV/STREAM/00002.m2ts', (24, None), preset=[PresetBD, PresetAAC])
+E14 = FileInfo(BDMV / 'JUJUTSUKAISEN_5/BDMV/STREAM/00003.m2ts', (26, None), preset=[PresetBD, PresetAAC])
+E15 = FileInfo(BDMV / 'JUJUTSUKAISEN_5/BDMV/STREAM/00004.m2ts', (26, -27),  preset=[PresetBD, PresetAAC])
 
-E16 = FileInfo(fr'{BDMV}/JUJUTSUKAISEN_6/BDMV/STREAM/00002.m2ts', (24, None), preset=[PresetBD, PresetAAC])
-E17 = FileInfo(fr'{BDMV}/JUJUTSUKAISEN_6/BDMV/STREAM/00003.m2ts', (26, None), preset=[PresetBD, PresetAAC])
-E18 = FileInfo(fr'{BDMV}/JUJUTSUKAISEN_6/BDMV/STREAM/00004.m2ts', (26, -24),  preset=[PresetBD, PresetAAC])
+E16 = FileInfo(BDMV / 'JUJUTSUKAISEN_6/BDMV/STREAM/00002.m2ts', (24, None), preset=[PresetBD, PresetAAC])
+E17 = FileInfo(BDMV / 'JUJUTSUKAISEN_6/BDMV/STREAM/00003.m2ts', (26, None), preset=[PresetBD, PresetAAC])
+E18 = FileInfo(BDMV / 'JUJUTSUKAISEN_6/BDMV/STREAM/00004.m2ts', (26, -24),  preset=[PresetBD, PresetAAC])
 
-E19 = FileInfo(fr'{BDMV}/JUJUTSUKAISEN_7/BDMV/STREAM/00002.m2ts', (24, None), preset=[PresetBD, PresetAAC])
-E20 = FileInfo(fr'{BDMV}/JUJUTSUKAISEN_7/BDMV/STREAM/00003.m2ts', (27, None), preset=[PresetBD, PresetAAC])
-E21 = FileInfo(fr'{BDMV}/JUJUTSUKAISEN_7/BDMV/STREAM/00004.m2ts', (27, -27),  preset=[PresetBD, PresetAAC])
+E19 = FileInfo(BDMV / 'JUJUTSUKAISEN_7/BDMV/STREAM/00002.m2ts', (24, None), preset=[PresetBD, PresetAAC])
+E20 = FileInfo(BDMV / 'JUJUTSUKAISEN_7/BDMV/STREAM/00003.m2ts', (27, None), preset=[PresetBD, PresetAAC])
+E21 = FileInfo(BDMV / 'JUJUTSUKAISEN_7/BDMV/STREAM/00004.m2ts', (27, -27),  preset=[PresetBD, PresetAAC])
 
-E22 = FileInfo(fr'{BDMV}/JUJUTSUKAISEN_8/BDMV/STREAM/00002.m2ts', (24, None), preset=[PresetBD, PresetAAC])
-E23 = FileInfo(fr'{BDMV}/JUJUTSUKAISEN_8/BDMV/STREAM/00003.m2ts', (24, None), preset=[PresetBD, PresetAAC])
-E24 = FileInfo(fr'{BDMV}/JUJUTSUKAISEN_8/BDMV/STREAM/00004.m2ts', (26, -25),  preset=[PresetBD, PresetAAC])
+E22 = FileInfo(BDMV / 'JUJUTSUKAISEN_8/BDMV/STREAM/00002.m2ts', (24, None), preset=[PresetBD, PresetAAC])
+E23 = FileInfo(BDMV / 'JUJUTSUKAISEN_8/BDMV/STREAM/00003.m2ts', (24, None), preset=[PresetBD, PresetAAC])
+E24 = FileInfo(BDMV / 'JUJUTSUKAISEN_8/BDMV/STREAM/00004.m2ts', (26, -25),  preset=[PresetBD, PresetAAC])
 
-# Masks
-
-OP1E_MASK = FileInfo(fr'{BDMV}/masks/OP1E.png')
-E21A_MASK = FileInfo(fr'{BDMV}/masks/E21A.png')
-
-LETTERBOX_MASK_A = FileInfo(fr'{BDMV}/masks/LETTERBOX_A.png')
-LETTERBOX_MASK_B = FileInfo(fr'{BDMV}/masks/LETTERBOX_B.png')
 
 # Bonus
 
-OP1  = FileInfo(fr'{BDMV}/JUJUTSUKAISEN_1/BDMV/STREAM/00005.m2ts', (0, -24), preset=[PresetBD, PresetAAC])
-ED1  = FileInfo(fr'{BDMV}/JUJUTSUKAISEN_1/BDMV/STREAM/00006.m2ts', (0, -24), preset=[PresetBD, PresetAAC])
-OP2  = FileInfo(fr'{BDMV}/JUJUTSUKAISEN_5/BDMV/STREAM/00005.m2ts', (0, -24), preset=[PresetBD, PresetAAC])
-ED2  = FileInfo(fr'{BDMV}/JUJUTSUKAISEN_5/BDMV/STREAM/00018.m2ts', (0, -24), preset=[PresetBD, PresetAAC])
+OP1  = FileInfo(BDMV / 'JUJUTSUKAISEN_1/BDMV/STREAM/00005.m2ts', (0, -24), preset=[PresetBD, PresetAAC])
+ED1  = FileInfo(BDMV / 'JUJUTSUKAISEN_1/BDMV/STREAM/00006.m2ts', (0, -24), preset=[PresetBD, PresetAAC])
+OP2  = FileInfo(BDMV / 'JUJUTSUKAISEN_5/BDMV/STREAM/00005.m2ts', (0, -24), preset=[PresetBD, PresetAAC])
+ED2  = FileInfo(BDMV / 'JUJUTSUKAISEN_5/BDMV/STREAM/00018.m2ts', (0, -24), preset=[PresetBD, PresetAAC])
 
-OP1B = FileInfo(fr'{BDMV}/JUJUTSUKAISEN_1/BDMV/STREAM/00010.m2ts', (0, -24), preset=[PresetBD, PresetAAC])
-OP1C = FileInfo(fr'{BDMV}/JUJUTSUKAISEN_2/BDMV/STREAM/00005.m2ts', (0, -24), preset=[PresetBD, PresetAAC])
-OP1D = FileInfo(fr'{BDMV}/JUJUTSUKAISEN_3/BDMV/STREAM/00005.m2ts', (0, -24), preset=[PresetBD, PresetAAC])
+OP1B = FileInfo(BDMV / 'JUJUTSUKAISEN_1/BDMV/STREAM/00010.m2ts', (0, -24), preset=[PresetBD, PresetAAC])
+OP1C = FileInfo(BDMV / 'JUJUTSUKAISEN_2/BDMV/STREAM/00005.m2ts', (0, -24), preset=[PresetBD, PresetAAC])
+OP1D = FileInfo(BDMV / 'JUJUTSUKAISEN_3/BDMV/STREAM/00005.m2ts', (0, -24), preset=[PresetBD, PresetAAC])
+OP1E = FileInfo(BDMV / 'JUJUTSUKAISEN_3/BDMV/STREAM/00005.m2ts', (0, -24), preset=[PresetBD, PresetAAC])
 
-OP1E = FileInfo(fr'{BDMV}/JUJUTSUKAISEN_3/BDMV/STREAM/00005.m2ts', (0, -24), preset=[PresetBD, PresetAAC])
+OP1E_MASK = FileInfo(BDMV / 'masks/OP1E.png')
 OP1E.clip_cut = recreate_OP1E(E13, OP1D, OP1E_MASK)
 
